678c1a4011247d53b7538fffdbcfc9ad5fbece65
Elvin Aslanov <rwp.primary@gmail.com>
2024-09-24 19:36:18 +0200

    Major refactor: PowerShell

--- brightness.ps1
+++ brightness.ps1
@@ -1,33 +1,35 @@
 param(
-  [switch] $Night,
-  [switch] $Day,
-  [switch] $Middle
+  [ switch ] $Night ,
+  [ switch ] $Day ,
+  [ switch ] $Middle
 )
 
-if ($Night) {
-  $brightness = 25
-  $contrast = 50
-  $brightness_laptop=50
-} elseif ($Day) {
-  $brightness = 50
-  $contrast = 70
-  $brightness_laptop=75
-} elseif ($Middle) {
-  $brightness = 35
-  $contrast = 55
-  $brightness_laptop=60
-} else {
-  Write-Output 'Please specify an option'
-}
+function set_brightness ( [ int ] $Brightness , [ int ] $Contrast , [ int ] $Laptop , [ string ] $Color = 'Cool' ) {
 
-ddm /readbrightnesslevel `
+  ddm /readbrightnesslevel `
     /readcontrastlevel `
-    /writebrightnesslevel $brightness `
-    /writecontrastlevel $contrast `
-    /writecolorpreset $color `
+    /writebrightnesslevel $Brightness `
+    /writecontrastlevel $Contrast `
+    /writecolorpreset $Color `
     /console start
 
-$wmi = Get-WmiObject -Namespace root/WMI `
+  $wmi = Get-WmiObject -Namespace root\wmi `
         -Class WmiMonitorBrightnessMethods
 
-$wmi.WmiSetBrightness(1, $brightness_laptop)
+  $wmi.WmiSetBrightness( 1 , $Laptop )
+
+}
+
+if ( $Night ) {
+  set_brightness -Brightness 25 -Contrast 50 -Laptop 50
+} elseif ($Day) {
+  set_brightness -Brightness 50 -Contrast 70 -Laptop 75
+} elseif ($Middle) {
+  set_brightness -Brightness 35  -Contrast 55  -Laptop 60
+} else {
+  Write-Output 'Please specify an option'
+  $choice = Read-Host -Prompt 'Choice: '
+  if ( $choice -eq 'night' ) {
+    set_brightness -Brightness 25 -Contrast 50 -Laptop 50
+  }
+}
